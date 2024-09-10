890925450be210211aeca91bc480bb67b31bf5a7
nielsbeuk <niels@beukenkamp.net>
2024-09-10 13:56:08 +0200

    Update api_management_resource.go
    At our company we have the possibility to use 32 units in APIM in our main and additional locations. The additional location can now only be scaled to 12

--- internal/services/apimanagement/api_management_resource.go
+++ internal/services/apimanagement/api_management_resource.go
@@ -209,7 +209,7 @@ func resourceApiManagementSchema() map[string]*pluginsdk.Schema {
 						Type:         pluginsdk.TypeInt,
 						Optional:     true,
 						Computed:     true,
-						ValidateFunc: validation.IntBetween(0, 12),
+						ValidateFunc: validation.IntBetween(0, 32),
 					},
 
 					"zones": commonschema.ZonesMultipleOptional(),
